c4e06824b4321e9c5e6c4daac7b3c7984fcbd666
willywin99 <willywinsianturi@gmail.com>
2022-07-16 00:34:28 +0700

    Add Images and Padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wiki_ball/detail_screen.dart';
  
 void main() => runApp(MyApp());
  
@@ -9,69 +10,6 @@ class MyApp extends StatelessWidget {
       title: 'Wiki Ball',
       theme: ThemeData(),
       home: DetailScreen(),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: 16.0),
-            child: Text(
-              'Manchester United',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Icon(Icons.emoji_events_outlined),
-                    SizedBox(height: 8.0),
-                    Text('England'),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Icon(Icons.stadium_outlined),
-                    SizedBox(height: 8.0),
-                    Text('Old Trafford'),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Icon(Icons.home_work_outlined),
-                    SizedBox(height: 8.0),
-                    Text('1878'),
-                  ],
-                ),
-              ]
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'Manchester United Footbal Club adalah sebuah klub sepak bola profesional yang berbasis di Old Trafford, Manchester Raya, yang bermain di Liga Utama Inggris dengan piala Liga Utama Inggris terbanyak sepanjang masa.',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16.0),
-            ),
-          ),
-        ],
-      ),
-      ),
     );
   }
 }
